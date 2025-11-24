094a813c6fe9ac81b9fa308b14e66091d6802b81
Karim Mahmoud <128514567+Karimgebril09@users.noreply.github.com>
2025-11-24 21:18:29 +0200

    changes in implementation

--- LSH.py
+++ LSH.py
@@ -25,10 +25,11 @@ def Build_LSH_index(index_path:str,dataset_vectors: np.ndarray, num_planes: int)
     plane_norms = orthogonal_planes(num_planes, DIMENSION) 
     hash_buckets = {}   
     Projections=np.dot(dataset_vectors, plane_norms.T)
-    hash_bits = (Projections > 0).astype(int)
+    hash_bits = (Projections > 0).astype(np.int32)
     
     for i in range(len(dataset_vectors)):
-        hash_int = hash_bits[i].dot(1 << np.arange(num_planes)[::-1])
+        hash_int = int("".join(map(str, hash_bits[i])), 2)
+        
         if hash_int not in hash_buckets:
             hash_buckets[hash_int] = []
         hash_buckets[hash_int].append(i)
@@ -55,14 +56,12 @@ def Build_LSH_index(index_path:str,dataset_vectors: np.ndarray, num_planes: int)
 
 
 def retreive_LSH(Plane_norms: np.ndarray, query_vector: np.ndarray, index_path: str):
-    if query_vector.ndim == 1:
-        query_vector = query_vector.reshape(1, -1)
     dot_products = np.dot(query_vector, Plane_norms.T)
-    hash_bits = (dot_products > 0).astype(int)[0]
-    hash_str = ''.join(hash_bits.astype(str))
+    hash_bits = (dot_products > 0).astype(np.int32)[0]
+    hash_int = int("".join(map(str, hash_bits)), 2)
 
     indices = []
-    bucket_file = os.path.join(index_path, f"{hash_str}.dat")
+    bucket_file = os.path.join(index_path, f"{hash_int}.dat")
     if os.path.exists(bucket_file):
         indices = np.memmap(bucket_file, dtype=np.uint32, mode='r')
         return indices
@@ -73,7 +72,7 @@ def retreive_LSH(Plane_norms: np.ndarray, query_vector: np.ndarray, index_path: 
         if file == "plane_norms.dat":
             continue
         current_hash=file.split(".npy")[0]
-        distance=HammingDistance(hash_str, current_hash)
+        distance=HammingDistance(hash_int, int(current_hash))
         if distance<min_distance:
             min_distance=distance
             closest_hash=current_hash
